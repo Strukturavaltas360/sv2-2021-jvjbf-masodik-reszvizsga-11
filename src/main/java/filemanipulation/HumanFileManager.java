c27655ed8ab26fad3542140ef30d0ccc2ffa1205
ht0m <hartht@gmail.com>
2021-12-13 14:41:45 +0100

    HumanFileManager

---
+++ src/main/java/filemanipulation/HumanFileManager.java
@@ -0,0 +1,60 @@
+package filemanipulation;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+public class HumanFileManager {
+
+    private List<Human> humans = new ArrayList<>();
+    private List<String> lines = new ArrayList<>();
+
+
+    public List<Human> readHumansFromFile(Path path) {
+        try {
+            lines = Files.readAllLines(path);
+        } catch (IOException ioe) {
+            throw new IllegalStateException("Can't read file!", ioe);
+        }
+
+        for (String actual : lines) {
+            humans.add(geHumanFromString(actual));
+        }
+        return humans;
+    }
+
+    private Human geHumanFromString(String line) {
+        String[] s = line.split(";");
+        return new Human(s[0], s[1]);
+    }
+
+    private boolean isHumanAMale(Human human) {
+        if (human.getIdentityNumber().charAt(0) == '1' || human.getIdentityNumber().charAt(0) == '3') {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+
+    public void writeMaleHumansToFile(Path path) {
+        List<String> males = new ArrayList<>();
+        for (String actual : lines) {
+            if (isHumanAMale(geHumanFromString(actual))) {
+                males.add(actual);
+            }
+        }
+        try {
+            Files.write(path, males);
+        } catch (IOException ioe) {
+            throw new IllegalStateException("Error writing file!");
+        }
+    }
+
+    public List<Human> getHumans() {
+        return humans;
+    }
+}
